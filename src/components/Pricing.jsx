84aefbb446280420f7b822ca31a440ed6f71db5e
Muhammed Anas M A <muhammedanas6282@gmail.com>
2025-05-31 22:34:32 +0530

    updated

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,8 @@
 import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
 import Section from "./Section";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
@@ -17,8 +20,8 @@ const Pricing = () => {
             alt="sphere"
           />
           <div
-            className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2
-          -translate-y-1/2 pointer-events-none"
+            className="absolute top-1/2 left-1/2 w-[60rem]
+             -translate-x-1/2 -translate-y-1/2 pointer-events-none"
           >
             <img
               src={stars}
@@ -28,6 +31,25 @@ const Pricing = () => {
               alt="Stars"
             />
           </div>
+        </div>
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever"
+        />
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold
+          tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
         </div>
       </div>
     </Section>
